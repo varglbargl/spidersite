2dead9c588b7f3076f17fd89ac1f59fa297be6bb
Eric <ehannum@gmail.com>
2017-11-19 18:27:40 -0800

    switched from npm to yarn for some reason

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function(grunt) {
     concat: {
       dist: {
         src: [
+          'node_modules/jquery/dist/jquery.min.js',
           'src/spidersite.js'
         ],
         dest: 'build/build.js'
